1bf558a177b988752e3cd6060202d702b4df9222
pmosk <pm@ggroupp.ru>
2022-06-30 20:31:16 +0300

    Encode HTML with text style

---
+++ src/Extensions/Extensions.TurnContext/Extensions.EncodeHtmlTextWithStyle.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Web;
+using Microsoft.Bot.Builder;
+
+namespace GGroupp.Infra.Bot.Builder;
+
+public static partial class TurnContextExtensions
+{
+    public static string EncodeHtmlTextWithStyle(this ITurnContext? turnContext, [AllowNull] string source, BotTextStyle style)
+    {
+        if (string.IsNullOrEmpty(source))
+        {
+            return string.Empty;
+        }
+
+        if (turnContext is null || turnContext.IsNotTelegramChannel())
+        {
+            return InnerBuildTextWithStyle(source ?? string.Empty, style);
+        }
+
+        var encodedString = HttpUtility.HtmlEncode(source ?? string.Empty);
+        return InnerBuildHtmlTextWithStyle(encodedString, style);
+    }
+
+    private static string InnerBuildHtmlTextWithStyle(string text, BotTextStyle style)
+        =>
+        style switch
+        {
+            BotTextStyle.Bold => string.Concat("<b>", text, "</b>"),
+            BotTextStyle.Italic => string.Concat("<i>", text, "</i>"),
+            _ => text
+        };
+}
