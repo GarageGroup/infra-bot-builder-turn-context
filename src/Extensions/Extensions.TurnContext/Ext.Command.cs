c2bc7ab60ed1e6a8f5800eeaf0933ccac0e70d1f
pmosk <pm@ggroupp.ru>
2024-04-24 18:35:44 +0400

    Use .NET8 only

--- src/Extensions/Extensions.TurnContext/Ext.Command.cs
+++ src/Extensions/Extensions.TurnContext/Ext.Command.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
@@ -9,17 +8,12 @@ namespace GarageGroup.Infra.Bot.Builder;
 partial class TurnContextExtensions
 {
     public static Optional<string> RecognizeCommandOrAbsent(this ITurnContext? turnContext, params string[] commandNames)
-        =>
-        InnerRecognizeCommandOrAbsent(
-            turnContext, commandNames ?? Array.Empty<string>());
-
-    private static Optional<string> InnerRecognizeCommandOrAbsent(this ITurnContext? turnContext, IReadOnlyCollection<string> commandNames)
     {
         return turnContext.InnerParseCommandOrAbsent().Filter(IsCommandMatch);
 
         bool IsCommandMatch(string text)
             =>
-            commandNames.Any() is false || commandNames.Contains(text, StringComparer.InvariantCultureIgnoreCase);
+            commandNames.Length is not > 0 || commandNames.Contains(text, StringComparer.InvariantCultureIgnoreCase);
     }
 
     private static Optional<string> InnerParseCommandOrAbsent(this ITurnContext? turnContext)
